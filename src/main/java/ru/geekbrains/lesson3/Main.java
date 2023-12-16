e64878aca0c8f78eaeab739f42c86f37ddf86e7b
KiTSune MiDSun <118630098+DeleteDone@users.noreply.github.com>
2023-12-16 11:09:00 +0300

    Update Main.java

--- src/main/java/ru/geekbrains/lesson3/Main.java
+++ src/main/java/ru/geekbrains/lesson3/Main.java
@@ -3,10 +3,10 @@ package ru.geekbrains.lesson3;
 public class Main {
     public static void main(String[] args) {
         Worker[] workers = new Worker[4];
-        workers[0] = new HourlyWorker("Ivanov", 100);
-        workers[1] = new HourlyWorker("Petrov", 120);
-        workers[2] = new FixedWorker("Sidorov", 5000);
-        workers[3] = new FixedWorker("Kuznetsov", 7000);
+        workers[0] = new HourlyWorker("W1", 100);
+        workers[1] = new HourlyWorker("W2", 120);
+        workers[2] = new FixedWorker("W3", 5000);
+        workers[3] = new FixedWorker("W4", 7000);
 
         // Создание объекта класса WorkersArray
         WorkersArray workersArray = new WorkersArray(workers);
