c157ef9e1a350ffe8715869720dc0761610d307d
KiTSune MiDSun <118630098+DeleteDone@users.noreply.github.com>
2023-12-19 17:07:38 +0300

    Delete src/main/ClassWork/lesson4 directory

--- src/main/ClassWork/lesson4/src/main/java/ru/geekbrains/lesson4/task1/MyArraySizeException.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.lesson4.task1;
-
-public class MyArraySizeException extends CustomArrayException{
-    public MyArraySizeException(String message, int x, int y) {
-        super(message, x, y);
-    }
-}
